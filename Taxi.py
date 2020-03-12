e3d10c6761aa09b5aa9a6c6275ef7b7e3622b990
kevin <kevincoppens03051999@gmail.com>
2020-03-12 14:52:19 +0100

    Code monkey doing coding stuff

--- Taxi.py
+++ Taxi.py
@@ -16,7 +16,7 @@ def callme():
         exit()
     else:
         r.raw.decode_content = True
-        with open("file1.csv", 'wb') as f:
+        with open(r"C:\Users\Kevin\Desktop\Data\File1.csv", 'wb') as f:
             shutil.copyfileobj(r.raw, f)
         print( "Success")
 
@@ -37,10 +37,45 @@ def test():
     stuff_in_string = "Shepherd {} is {} years old.".format(shepherd, age)
     print(stuff_in_string)
 
+def test2():
+    jaar = 9
+    jaartal = '%02d' % jaar
+    maand = 0
+    maandtal = '%02d' % maand
+
+    while jaartal != "11":
+        if maandtal == "12":
+            maand = 0
+            jaar = jaar + 1
+            maandtal = '%02d' % maand
+            jaartal = '%02d' % jaar
+        else:
+            maand = maand + 1
+            maandtal = '%02d' % maand
+            print(jaartal,maandtal)
+
+
+        #als er dan bij de if inplaats van exit moet hij eerst de laatste combinatie opslaan en dan deze weer terughalen.
+
+
+
+    #url = "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2009-01.csv"
+    #r = requests.get(url, verify=False, stream=True)
+    #if r.status_code!=200:
+    #    print( "Failure!!")
+    #    exit()
+    #else:
+    #    r.raw.decode_content = True
+    #    with open(r"C:\Users\Kevin\Desktop\Data\File1.csv", 'wb') as f:
+    #        shutil.copyfileobj(r.raw, f)
+    #    print( "Success")
+
+
 if __name__ == '__main__':
     #callme()
     #databaseschrijven()
-    test()
+    #test()
+    test2()
 
 
 eind = time()
