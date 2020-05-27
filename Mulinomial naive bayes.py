4ecf4267cf59607b1af85d23097936e5a6cc48f9
kevin <kevincoppens03051999@gmail.com>
2020-05-27 13:40:51 +0200

    Code monkey doing coding stuff

---
+++ Mulinomial naive bayes.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import re
+import nltk.corpus
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
+import spacy
+import numpy as np
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing,svm,tree
+from sklearn.naive_bayes import GaussianNB,MultinomialNB,ComplementNB,BernoulliNB,CategoricalNB
+
+#hier laden wij voor nltk library's in
+nlp = spacy.load('en')
+stop = stopwords.words('english')
+
+#hier laden wij de data in
+test_df = pd.read_csv(r"C:\Users\Kevin\Desktop\test.csv",low_memory=False)
+train_data = pd.read_csv(r"C:\Users\Kevin\Desktop\train.csv", low_memory=False)
+
+#hier verwijderen wij kolommen
+pd.set_option('display.max_colwidth', -1)
+cols_to_drop = ['id', 'keyword', 'location']
+train_data = train_data.drop(cols_to_drop, axis=1)
+
+#tussentijds resultaat
+print(train_data.head())
+
+#hier zetten wij alles om naar lower character. en halen wij alle gekke tekens eruit
+train_data['text'] = train_data['text'].str.lower()
+train_data['text'] = train_data['text'].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))
+train_data['text'] = train_data['text'].apply(lambda elem: re.sub(r"\d+", "", elem))
+
+#tussentijds resultaat
+print(train_data.head())
+
+#hier stellen wij de variabelen op om straks de tekst naar vectors om te zetten
+count_vectorizer = feature_extraction.text.CountVectorizer()
+#hier wordt onze train data omgezet naar vectors
+train_vectors = count_vectorizer.fit_transform(train_data["text"])
+#voorbeeld van de vectors
+print(train_vectors)
+#hier wordt onze test data omgezet naar vectors
+test_vectors = count_vectorizer.transform(test_df["text"])
+
+#hier halen wij al onze stopwoorden weg
+train_data['text'] = train_data['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+
+clf = MultinomialNB()
+scores = model_selection.cross_val_score(clf, train_vectors, train_data["target"], cv=3, scoring="f1")
+print(scores)
+
--- Versie 1.py
+++ Versie 1.py
@@ -8,11 +8,16 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
 import spacy
+import numpy as np
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing,svm,tree
+from sklearn.naive_bayes import GaussianNB,MultinomialNB,ComplementNB,BernoulliNB,CategoricalNB
+
 
 nlp = spacy.load('en')
 stop = stopwords.words('english')
 
 
+test_df = pd.read_csv(r"C:\Users\Kevin\Desktop\test.csv",low_memory=False)
 train_data = pd.read_csv(r"C:\Users\Kevin\Desktop\train.csv", low_memory=False)
 
 pd.set_option('display.max_colwidth', -1)
@@ -35,9 +40,11 @@ train_data['text'] = train_data['text'].apply(lambda x: ' '.join([word for word 
 
 print(train_data.head())
 
-train_data['text'] = train_data['text'].apply(lambda x: word_tokenize(x))
 
-print(train_data.head())
+#train_data['text'] = train_data['text'].apply(lambda x: word_tokenize(x))
+
+#print(train_data.head())
+
 
 #def word_stemmer(text):
 #    stem_text = [PorterStemmer().stem(i) for i in text]
@@ -47,23 +54,58 @@ print(train_data.head())
 
 #print(train_data.head())
 
-def word_lemmatizer(text):
-    lem_text = [WordNetLemmatizer().lemmatize(i) for i in text]
-    return lem_text
 
-train_data['text'] = train_data['text'].apply(lambda x: word_lemmatizer(x))
+#def word_lemmatizer(text):
+#    lem_text = [WordNetLemmatizer().lemmatize(i) for i in text]
+#    return lem_text
 
-print(train_data.head())
+#train_data['text'] = train_data['text'].apply(lambda x: word_lemmatizer(x))
 
-def word_pos_tagger(text):
-    pos_tagged_text = nltk.pos_tag(text)
-    return pos_tagged_text
+#print(train_data.head())
 
-train_data['text'] = train_data['text'].apply(lambda x: word_pos_tagger(x))
 
-print(train_data.head())
+#def word_pos_tagger(text):
+#    pos_tagged_text = nltk.pos_tag(text)
+#    return pos_tagged_text
+
+#train_data['text'] = train_data['text'].apply(lambda x: word_pos_tagger(x))
+
+#print(train_data.head())
+
 
 #text = nlp("forest fire near la ronge sask canada")
-text = nlp("deeds reason earthquake may allah forgive us")
-for chunk in text.noun_chunks:
-    print(chunk.text, chunk.label_, chunk.root.text)
+#text = nlp("deeds reason earthquake may allah forgive us")
+#for chunk in text.noun_chunks:
+#    print(chunk.text, chunk.label_, chunk.root.text)
+
+
+count_vectorizer = feature_extraction.text.CountVectorizer()
+
+train_vectors = count_vectorizer.fit_transform(train_data["text"])
+
+test_vectors = count_vectorizer.transform(test_df["text"])
+
+#gebruik van linear model
+#clf = linear_model.RidgeClassifier()
+
+#gebruik van een support vector machine
+#clf = svm.SVC()
+
+#decisiontree
+#clf = tree.DecisionTreeClassifier()
+
+#Multinomial Naive Bayes
+#De beste tot nu toe score = 0.66,0.64.0.71
+clf = MultinomialNB()
+
+#Complement naive Bayes
+# net iets slechter dan multinomial
+#clf = ComplementNB()
+
+#Bernoulli Naive bayes
+#score = 0.64,0.60,0.73
+#clf = BernoulliNB()
+
+scores = model_selection.cross_val_score(clf, train_vectors, train_data["target"], cv=3, scoring="f1")
+print(scores)
+
